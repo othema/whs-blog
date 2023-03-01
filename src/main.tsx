960c1e2d9e363f1f376ac8e8ba69f6dfb1d8f806
othema <78880488+othema@users.noreply.github.com>
2023-03-01 12:47:00 +0000

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
-import Layout from "./Layout";
+import Layout from './Layout';
 import BlogPage from './routes/BlogPage';
 import LoginPage from './routes/LoginPage';
 import StudyContentPage from './routes/StudyContentPage';
 import ViewBlogPage from './routes/ViewBlogPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<Layout />}>
-				<Route index element={<Navigate to="/blog" replace />} />
-				<Route path="blog">
-					<Route index element={<BlogPage />} />
-					<Route path=":blog" element={<ViewBlogPage />} />
-				</Route>
+  <BrowserRouter>
+    <Routes>
+      <Analytics />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/blog" replace />} />
+        <Route path="blog">
+          <Route index element={<BlogPage />} />
+          <Route path=":blog" element={<ViewBlogPage />} />
+        </Route>
 
-				<Route path="study" element={<StudyContentPage />} />
-				<Route path="login" element={<LoginPage />} />
+        <Route path="study" element={<StudyContentPage />} />
+        <Route path="login" element={<LoginPage />} />
 
-				<Route path="*" element={404} />
-			</Route>
-		</Routes>
-	</BrowserRouter>
-)
+        <Route path="*" element={404} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
